273184c3c062c90f4fc28c69570be4239876e472
Alexsandro G. Pereira <alexsandro.pereira@gmail.com>
2023-06-13 23:42:00 -0300

    first commit

---
+++ README..md
@@ -0,0 +1,20 @@
+# DevLinks
+
+![preview](./.github/tela-apresentacao.jpg)
+
+> Links para Contatos da Lifeweb.
+
+Projeto construído com links para redes sociais e outros.
+
+[🔗 Clique aqui para acessar](https://lifeweb.com.br/links/)
+
+## 🛠 Tecnologias
+
+- HTML
+- CSS
+- Git e Github
+- JavaScript
+
+## Feito com muito 💛 Contato Lifeweb
+
+alexsandro.pereira@gmail.com
---
+++ script.js
@@ -0,0 +1,32 @@
+function toggleMode() {
+  const html = document.documentElement
+  /*
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+  */
+  // MESMO RESULTADO QUE A FUNÇÃO ACIMA.
+  html.classList.toggle("light")
+
+  //Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //SUBSTITUIR IMAGEM
+  if (html.classList.contains("light")) {
+    //se tiver Light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mykon Brinto com fundo azul, usando óculos escuro e jaqueta preta"
+    )
+  } else {
+    // senão tiver no mode light, manter a foto normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mykon Brinto com fundo azul, usando óculos e jaqueta preta"
+    )
+  }
+}
